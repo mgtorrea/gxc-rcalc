eccfadfb80d4376507f9cf8b618da7d46758c4db
fcisneros <fernando.cisneros@sm4rt.com>
2015-11-09 22:54:50 -0600

    Partial refactor of ApestanCrawler as flume source

---
+++ crawlers/src/main/java/gxc/rcalc/crawlers/apestan/ApestanCrawlerSource.java
@@ -0,0 +1,127 @@
+package gxc.rcalc.crawlers.apestan;
+
+import gxc.rcalc.crawlers.CrawlUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.EventDeliveryException;
+import org.apache.flume.FlumeException;
+import org.apache.flume.PollableSource;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.AbstractSource;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * CrawlerTask implementation for apestan.com site crawling
+ * 
+ * @author fcisneros
+ *
+ */
+public class ApestanCrawlerSource extends AbstractSource implements Configurable, PollableSource {
+	
+	public static final String COMPANIES_URL = 
+			"https://www.apestan.com/lista-completa-de-companias.html/period_year";
+	
+	private Iterator<Element> companies;
+	
+	@Override
+	public void configure(Context ctx) {
+		
+	}
+	
+	@Override
+	public void start() {
+		Document doc;
+		try {
+			doc = Jsoup.connect(COMPANIES_URL).userAgent("Mozilla").get();
+			// Extract company elements
+			this.companies = doc.select("div#content ul.userList li").iterator();
+			
+		} catch (IOException e) {
+			throw new FlumeException("Could not retrieve Companies list",e);
+		}
+	}
+	
+	@Override
+	public void stop() {
+		
+	}
+
+	@Override
+	public Status process() throws EventDeliveryException {
+		if(!companies.hasNext()) {
+			stop(); // is manual stop valid ?
+		}
+		
+		Status status = null;
+
+		try {
+			// Poll data
+			
+			Event e = crawlNextCompany();
+
+			// Store the Event into this Source's associated Channel(s)
+			getChannelProcessor().processEvent(e);
+
+			status = Status.READY;
+		} catch (Throwable t) {
+			status = Status.BACKOFF;
+			
+			// re-throw all Errors
+			if (t instanceof Error) {
+				throw (Error) t;
+			}
+		} finally {
+			
+		}
+		
+		return status;	
+	}
+	
+	private Event crawlNextCompany() throws IOException {
+		Map<String,Object> map = crawlCompany(companies.next());
+		
+		// TODO: serialize company Map
+		Event e = new EventBuilder().withBody("".getBytes());
+		return e;
+	}
+	
+	private Map<String,Object> crawlCompany(Element e) throws IOException {
+		Element link = e.select("a[href]").first();
+		String companyName = link.attr("title");		
+		String url = link.attr("abs:href");
+		List<Integer> ratings = crawlComplains(url);
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("companyName", companyName);
+		map.put("complainCount", ratings.size());
+		map.put("ratings", ratings);
+		
+		return map;
+	}
+	
+	private List<Integer> crawlComplains(String url) throws IOException {
+		Document doc = Jsoup.connect(url)
+				.userAgent("Mozilla")
+				.get();
+		
+		List<Integer> ratings = new ArrayList<Integer>();
+		for(Element e : doc.select("div.rating span")) {
+			Integer rating = CrawlUtils.extractInteger(e.className());
+			ratings.add((rating != null) ? rating : 0);
+		}
+
+		return ratings; // TODO: Add recursion for next links
+	}
+
+}
--- todo.md
+++ todo.md
@@ -6,9 +6,8 @@ TODO
 	* add risk index	(done)
 * complains front-end interface
 * improve risk formula
-* complete apestan.com crawler
+* complete apestan.com crawler and refactor ir as flume sources
 * implement risk index calculation using complains and crawlers' data
 	* define storage impl
 	* update risk index after calculation via admin endpoint 
-* add other crawlers
 * enable heroku deployment for rest module	(done)
